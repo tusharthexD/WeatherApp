4dc642202bf896e051fcc38e3b4f430614a12a70
Tushar <tusharsuthar6@gmail.com>
2023-08-15 21:00:49 +0530

    Complete

---
+++ index.js
@@ -0,0 +1,60 @@
+import express from 'express'
+import axios from 'axios'
+import bodyParser from 'body-parser';
+import moment from 'moment/moment.js';
+const app = express()
+const port = 3000
+const apiKey = "d79d08b803e6a8b372671872b6e40c5c";
+
+app.use(express.static("public/"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+
+    res.render("index.ejs")})
+
+app.post('/', async (req, res) => {
+        try{
+            const response = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${req.body.city}&limit=5&appid=${apiKey}`)
+            res.render("index.ejs", {content: response.data})
+        }
+        catch (error){
+            res.send('Hello World!')}
+        })   
+        
+app.post('/city', async (req, res) => {
+  
+    const latLon = req.body.lat
+    // console.log(req.body.lat)
+    const [lat, lon] = latLon.split(',');
+
+try{
+
+const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+const result = response.data;
+const d = Date.now() + (result.timezone *1000);
+const p = new Date(d);
+const day = p.toUTCString();
+
+var dateString = day.split(' ').slice(0, 4).join(' ');
+var timeString = day.split(' ').slice(4, 5).join(' ');
+// console.log(dateString)
+// console.log(timeString)
+
+time(timeString)
+
+function time(time24){
+    const [sHours, minutes] = time24.match(/([0-9]{1,2}):([0-9]{2})/).slice(1);
+    const period = +sHours < 12 ? 'AM' : 'PM';
+    const hours = +sHours % 12 || 12;
+  
+     return `${hours}:${minutes} ${period}`;
+  }
+
+   res.render("weather.ejs", {content: result, date: dateString, time:time(timeString)})
+    }
+    catch (error){
+    res.send('error')}
+    })
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ public/app.js
@@ -0,0 +1,63 @@
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector(".loader").style.visibility = "visible";
+    } else {
+        document.querySelector(
+          ".loader").style.display = "none";
+        document.querySelector(
+          "body").style.visibility = "visible";
+          document.querySelector(".loader").style.animation = "fadeInAnimation ease 3s";
+          document.querySelector("img").style.animation = "fadeInAnimation ease 2s";
+    }
+};
+
+document.querySelector("input").addEventListener("click", slider)
+
+function slider(){
+    document.querySelector(".search").style.marginTop = "15vw";
+    
+}
+document.querySelector(".search").style.transition = "all 1s";
+function barposition(){
+    document.querySelector(".search").style.marginTop = "30px";
+}
+// background: -webkit-gradient(linear, left top, left bottom, from(#0d0177), to(#005eff)) fixed;
+
+
+
+
+
+var sunrise = document.querySelectorAll(".degcard")[2];
+var sunset = document.querySelectorAll(".degcard")[5]
+
+
+function sunRise(dt, timezone){
+    sunrise.querySelector("h3").innerText = sunLi(dt, timezone)
+
+}
+
+function sunSet(dt, timezone){
+    sunset.querySelector("h3").innerText = sunLi(dt, timezone)
+
+}
+
+function sunLi(dt, timezone){
+
+const d = dt*1000 + (timezone*1000);
+const p = new Date(d);
+const day = p.toUTCString()
+
+var timeString = day.split(' ').slice(4, 5).join(' ');
+
+return(time(timeString));
+
+function time(time24){
+    const [sHours, minutes] = time24.match(/([0-9]{1,2}):([0-9]{2})/).slice(1);
+    const period = +sHours < 12 ? 'AM' : 'PM';
+    const hours = +sHours % 12 || 12;
+  
+     return(`${hours}:${minutes} ${period}`);
+  }
+
+}
